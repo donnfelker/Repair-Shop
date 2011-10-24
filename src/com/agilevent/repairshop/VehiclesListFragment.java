1653afae801e76970f81ed44193e4147c2675144
donnfelker <donn@donnfelker.com>
2011-10-24 08:36:16 -0700

    Getting rid of Toast warning

--- src/com/agilevent/repairshop/VehiclesListFragment.java
+++ src/com/agilevent/repairshop/VehiclesListFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 
 import android.widget.ArrayAdapter;
-import android.widget.Toast;
 
 
 public class VehiclesListFragment extends ListFragment {
